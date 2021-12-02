192bc906ff7a2f909ea11d7c51d197fc80e2589b
vincellob <vincellob@yahoo.com>
2021-12-02 01:05:25 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import requests from './requests';
+import Row from './Row';
+import Banner from './Banner';
+import Nav from './Nav';
+import Footer from './Footer';
+
+
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Nav/>
+    <Banner/>
+    
+    <Row title="PRABHU'S ORIGINALS"  fetchUrl={requests.fetchNetflixOriginals}
+      isLargeRow={true}
+    />
+    <Row title="Trending Now"  fetchUrl={requests.fetchTrending}/>
+    <Row title="Top Rated"  fetchUrl={requests.fetchTopRated}/>
+    <Row title="Action Movies"  fetchUrl={requests.fetchActionMovies}/>
+    <Footer/>
+
+     
+    </div>
+  );
+}
+
+export default App;
